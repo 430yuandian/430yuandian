74b62f54dfb61d0c6a8d8ace8584c650e4fac9d5
430yuandian <a13660960559@qq.com>
2026-01-08 17:46:38 +0800

    Add files via upload

---
+++ music-config.js
@@ -0,0 +1,10 @@
+{
+    "songs": [
+        {
+            "title": "雪崩",
+            "artist": "王源",
+            "url": "assets/music/雪崩-王源.mp3",
+            "cover": "assets/music/covers/雪崩-cover.jpg"
+        }
+    ]
+}
